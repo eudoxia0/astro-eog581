d67cbde6a6cb1d7236c4913d92221880451bee00
Fernando Borretti <fernando@borretti.me>
2022-12-14 20:30:01 +1100

    Move tests

--- astro.lisp
+++ astro.lisp
@@ -30,9 +30,6 @@
   (print-unreadable-object (angle stream :type t)
     (humanize angle stream)))
 
-(let ((a (make-instance 'decimal-degrees :value 49.3)))
-  (assert (string= (princ-to-string a) "#<DECIMAL-DEGREES 49.3°>")))
-
 ;;; Hours-minutes-seconds (right ascension)
 
 (defclass hms-degrees ()
@@ -64,9 +61,6 @@
   (print-unreadable-object (angle stream :type t)
     (humanize angle stream)))
 
-(let ((a (make-instance 'hms-degrees :hours 7.2 :minutes 2.24 :seconds 1.42)))
-  (assert (string= (princ-to-string a) "#<HMS-DEGREES 7.2h2.2m1.4s>")))
-
 (defun hms-to-decimal (hms)
   "Convert an HMS (hours-minutes-seconds) angle to decimal degrees."
   (with-slots (hours minutes seconds) hms
@@ -74,13 +68,6 @@
                 (/ (* 15.0 minutes) 60.0)
                 (/ (* 15.0 seconds) 3600.0))))
       (make-instance 'decimal-degrees :value d))))
-
-(let ((hms (make-instance 'hms-degrees
-                          :hours 8.3
-                          :minutes 45.7
-                          :seconds 2.3)))
-  (let ((dec (hms-to-decimal hms)))
-    (assert (= (value dec) 135.93459))))
 
 ;;; Degrees-minutes-seconds (declination)
 
@@ -113,33 +100,6 @@
   (print-unreadable-object (angle stream :type t)
     (humanize angle stream)))
 
-(let ((a (make-instance 'dms-degrees :degrees 51.2 :minutes 2.24 :seconds 1.42)))
-  (assert (string= (princ-to-string a) "#<DMS-DEGREES 51.2°2.2m1.4s>")))
-
-(defun sign (x)
-  (cond ((< x 0.0)
-         -1)
-        ((= x 0.0)
-         0)
-        (t
-         1.0)))
-
-(defun dms-to-decimal (dms)
-  "Convert a DMS (degrees-minutes-seconds) angle to decimal degrees."
-  (with-slots (degrees minutes seconds) dms
-    (let ((d (* (sign degrees)
-                (+ (abs degrees)
-                   (/ minutes 60.0)
-                   (/ seconds 3600.0)))))
-      (make-instance 'decimal-degrees :value d))))
-
-(let ((dms (make-instance 'dms-degrees
-                          :degrees -19.6
-                          :minutes 45.7
-                          :seconds 2.3)))
-  (let ((dec (dms-to-decimal dms)))
-    (assert (= (value dec) -20.362307))))
-
 ;;;; Distances
 
 (defclass light-years ()
@@ -157,9 +117,6 @@
   (print-unreadable-object (d stream :type t)
     (humanize d stream)))
 
-(let ((a (make-instance 'light-years :value 23.4)))
-  (assert (string= (princ-to-string a) "#<LIGHT-YEARS 23.4ly>")))
-
 (defclass parsecs ()
   ((value :reader value
           :initarg :value
@@ -175,24 +132,15 @@
   (print-unreadable-object (d stream :type t)
     (humanize d stream)))
 
-(let ((a (make-instance 'parsecs :value 41.1)))
-  (assert (string= (princ-to-string a) "#<PARSECS 41.1pc>")))
-
 ;;; Conversion
 
 (defun light-years-to-parsecs (ly)
   "Convert the given distance in light years to parsecs."
   (make-instance 'parsecs :value (* (value ly) 0.306601)))
 
-(let ((ly (make-instance 'light-years :value 3.32)))
-  (assert (= (value (light-years-to-parsecs ly)) 1.0179152)))
-
 (defun parsecs-to-light-years (pc)
   "Convert the given distance in parsecs to light years."
   (make-instance 'light-years :value (* (value pc) 3.26156)))
-
-(let ((pc (make-instance 'parsecs :value 41.5)))
-  (assert (= (value (parsecs-to-light-years pc)) 135.35474)))
 
 ;;;; Star coordinates
 
--- test.lisp
+++ test.lisp
@@ -1,3 +1,67 @@
 ;;;;
 ;;;; Tests of the astronomy framework.
 ;;;;
+
+;;;; Angles
+
+;;; Decimal
+
+(let ((a (make-instance 'decimal-degrees :value 49.3)))
+  (assert (string= (princ-to-string a) "#<DECIMAL-DEGREES 49.3°>")))
+
+;;; HMs
+
+(let ((a (make-instance 'hms-degrees :hours 7.2 :minutes 2.24 :seconds 1.42)))
+  (assert (string= (princ-to-string a) "#<HMS-DEGREES 7.2h2.2m1.4s>")))
+
+(let ((hms (make-instance 'hms-degrees
+                          :hours 8.3
+                          :minutes 45.7
+                          :seconds 2.3)))
+  (let ((dec (hms-to-decimal hms)))
+    (assert (= (value dec) 135.93459))))
+
+;;; DMS
+
+(let ((a (make-instance 'dms-degrees :degrees 51.2 :minutes 2.24 :seconds 1.42)))
+  (assert (string= (princ-to-string a) "#<DMS-DEGREES 51.2°2.2m1.4s>")))
+
+(defun sign (x)
+  (cond ((< x 0.0)
+         -1)
+        ((= x 0.0)
+         0)
+        (t
+         1.0)))
+
+(defun dms-to-decimal (dms)
+  "Convert a DMS (degrees-minutes-seconds) angle to decimal degrees."
+  (with-slots (degrees minutes seconds) dms
+    (let ((d (* (sign degrees)
+                (+ (abs degrees)
+                   (/ minutes 60.0)
+                   (/ seconds 3600.0)))))
+      (make-instance 'decimal-degrees :value d))))
+
+(let ((dms (make-instance 'dms-degrees
+                          :degrees -19.6
+                          :minutes 45.7
+                          :seconds 2.3)))
+  (let ((dec (dms-to-decimal dms)))
+    (assert (= (value dec) -20.362307))))
+
+;;;; Distances
+
+(let ((a (make-instance 'light-years :value 23.4)))
+  (assert (string= (princ-to-string a) "#<LIGHT-YEARS 23.4ly>")))
+
+(let ((a (make-instance 'parsecs :value 41.1)))
+  (assert (string= (princ-to-string a) "#<PARSECS 41.1pc>")))
+
+(let ((ly (make-instance 'light-years :value 3.32)))
+  (assert (= (value (light-years-to-parsecs ly)) 1.0179152)))
+
+(let ((pc (make-instance 'parsecs :value 41.5)))
+  (assert (= (value (parsecs-to-light-years pc)) 135.35474)))
+
+;;; Conversion
